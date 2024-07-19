602316f06ac6415a8335ea8dd55694f6fd2ecbb0
Kaviya. T <90082734+kaviyathirumoorthy@users.noreply.github.com>
2024-07-19 23:49:17 +0530

    Update Main.java

--- SinglyLinkedList/Main.java
+++ SinglyLinkedList/Main.java
@@ -1,11 +1,18 @@
 public class Main {
 	public static void main(String[] args){
 		LinkedList list=new LinkedList();
-		list.addBeginning(10);list.addBeginning(20);list.addBeginning(30);list.addBeginning(40);
-		list.addBeginning(60);list.addBeginning(60);list.addEnd(5);list.addEnd(5);
+		list.addEnd(10);list.addEnd(20);list.addEnd(30);list.addEnd(40);
+		list.addEnd(60);
+                 LinkedList list2=new LinkedList();
+		list2.addEnd(2);list2.addEnd(20);list2.addEnd(30);list2.addEnd(41);
+		list2.addEnd(60);
 
+		LinkedList mergedd=new LinkedList();
+		mergedd.head= LinkedList.merged(list.head,list2.head);  //static method-merge
+		mergedd.display();
+		
 		list.display();
-		list.ElementFromLast(8,list);
+		list.ElementFromLast(2,list);
 		System.out.println(list.find(60));//find whether the key present in the list
 		list.RemoveDuplicatesFromSortedLinkedList();
 		list.reverse();
